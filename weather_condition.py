1c88b76a61ab7a90d840ab68960d869dd7d0e49f
Sulimanalmoneef8 <almoneef.s@gmail.com>
2026-07-02 16:29:27 +0300

    initial commit

---
+++ weather_condition.py
@@ -0,0 +1,7 @@
+def weather_condition():
+    print("Ther weather is pleasent in:", spring)
+    print("The weather is pleasent in", autumn)
+
+spring = "autumn"
+autumn = spring
+weather_condition()
